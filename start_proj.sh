c5e665a24ff51f7b2897d61afdb7bb071786ac8b
Kevin <kevinpt@yahoo.com>
2015-02-14 01:42:40 -0500

    Init script

---
+++ start_proj.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export MGC_WD=`pwd`
+
+# Create default modelsim.ini with altered library mapping
+if [ ! -f modelsim.ini ]; then
+  python scripts/init_modelsim.py
+fi
+
+# Create build directory and Modelsim libs if they don't exist
+if [ ! -d build ]; then
+  mkdir -p build/lib
+fi
